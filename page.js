5549d1d93028245add738309a891f01cc106807e
iqra30 <51120066+iqra30@users.noreply.github.com>
2019-07-13 01:45:27 +0500

    Add files via upload

---
+++ page.js
@@ -0,0 +1,50 @@
+let kolachi={
+    name: "Kolachi Resturant",
+    location: "Do Darya Karachi",
+    contact: "03213456778",
+    gMail: "Kolachi_resturant@gmail.com",
+    fbPage: "www.facebook.com/kolachi",
+    instagram: "Kolachiii",
+    twitter: "Khana peena@Kolachi",
+    branches: [Karachi,islamabad,lahore,hyderabad,rawalpindhi,quetta,peshawar],
+    owner: "xxxxxxx",
+    about: "we make food with passion",
+    partnership: [careem,aquafina,DHA],
+    membership: {
+        chargesPerMonth: 10000,
+        complimentStuff:[anytime-reservation,breakfast,hi-tea],
+        giveVouchers: true 
+    },
+    breakfast: {
+        parathas:[vegetable,cheese,pizza,nutella,plain,etc],
+        eggs:[boil,half-Fry,full-fry,omelette,raw-egg],
+    },
+    lunch: {
+        desi: [biryani,nihari,qurma,karahi,handi,paaye,pulao,etc],
+        fastFood: [burger,zinger-burger,fries,pizza,club-sandwich,rolls,etc],
+        bbq: [tikka,kebabs,malae-booti,bihari-booti,chops,etc],
+        chinese: [fried-rice-soups-shashly,puree,noodles,spagetti,etc],
+    },
+    dinner: "as you want",
+    mainCourses: {
+        handi:[beef,mutton,chicken,lamb,ostrish,deer],
+        karahi: [beef,chicken]
+    },
+    beverages: {
+        tea:[plain,kashmiri,elachi,loug,green-tea,lemon-tea,etc],
+        iceCream: "many flavours",
+        cocktails: "as you want",
+        sheesha: "many flavours"
+    },
+    newArrivals: [big-mac,double-dobblers,hot-tomato-soup,vegetable-biryani,nalli-biryani,more-to-come],
+    famousFor: "shahi handi and biryani",
+    goodService: true,
+    goodenvironment: true,
+    events: {
+        anniversary: "theme",
+        birthday: [complimentary-cake,theme],
+        getToGether: "tables arrange",
+        conserts: "seperat place",
+        yearlyFestivals:  
+    }
+}
